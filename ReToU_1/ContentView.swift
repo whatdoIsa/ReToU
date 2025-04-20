a8df190c28c8a6afd67a404f6a37d012d2dfce2e
whahdoIsa <gmlwns5504@naver.com>
2025-04-21 08:57:00 +0900

    Initial Commit

---
+++ ReToU_1/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ReToU_1
+//
+//  Created by Dean_SSONG on 4/21/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ReToU_1/ReToU_1App.swift
@@ -0,0 +1,17 @@
+//
+//  ReToU_1App.swift
+//  ReToU_1
+//
+//  Created by Dean_SSONG on 4/21/25.
+//
+
+import SwiftUI
+
+@main
+struct ReToU_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
